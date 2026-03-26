00eea111ce9b632dd0fee2c5aa45be21470b44f2
Praveen <praveenlokku9@gmail.com>
2026-03-26 09:52:39 +0530

    feat: Add ResumeService for managing resume-related operations.

--- src/main/java/com/resumebuilder/service/ResumeService.java
+++ src/main/java/com/resumebuilder/service/ResumeService.java
@@ -5,11 +5,13 @@ import com.resumebuilder.model.User;
 import com.resumebuilder.repository.ResumeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class ResumeService {
 
     @Autowired
